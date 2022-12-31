da67f792cf86f1beb1b9f81dbbdb0db7b3ab79a2
Pramote Siriparp <6210530017@student.tu.ac.th>
2022-12-31 09:27:25 +0700

    Connect to database

--- server.go
+++ server.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+
 	"github.com/6210530017/assessment/config"
+
+	_ "github.com/lib/pq"
 )
+
+func Setup(url string) (*sql.DB, func()) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses ( id SERIAL PRIMARY KEY, title TEXT, amount FLOAT, note TEXT, tags TEXT[]);
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	teardown := func() {
+		db.Close()
+	}
+
+	return db, teardown
+}
 
 func main() {
 	config := config.NewConfig()
 
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", config.Port)
-	fmt.Println("Database_URL: ", config.DB_url)
+	db, teardown := Setup(config.DB_url)
+	defer teardown()
+
+	fmt.Printf("%#v", db)
 }
